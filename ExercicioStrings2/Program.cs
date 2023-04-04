3e921223cfabd43aea581f3e39b9f4e1898fcc67
Joao Marcelo <joaoamarceloalves585@gmail.com>
2023-04-04 11:12:08 -0300

    Add project files.

---
+++ ExercicioStrings2/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ExercicioStrings2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+            Console.WriteLine("Digite a frase que você deseja saber a quantidade de letras: ");
+            Console.WriteLine();
+            String frase = "fala galera da academia";
+
+            string[] palavras = frase.Split(' ');
+            Console.WriteLine(palavras.Length);
+
+            Console.ReadLine();
+
+
+
+
+            ;
+
+
+
+
+
+        }
+    }
+}
